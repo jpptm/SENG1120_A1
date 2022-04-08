979a5669440524721d2ca523b79c546b134dd95a
jpptm <prncmontano@gmail.com>
2022-04-08 19:32:57 +1000

    .

--- Node.h
+++ Node.h
@@ -36,6 +36,6 @@ class Node{
     value_type node_data;
 };
 
-
+// overload < operator to use for typedef objects
 bool operator <(const Node::value_type& object_lhs, const Node::value_type& object_rhs);
 #endif // NODE_H
